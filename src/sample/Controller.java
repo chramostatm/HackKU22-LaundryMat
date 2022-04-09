f50f930a4e7aca8ca96edcc93849c3b31908a193
Matthew <rokusekm@lopers.unk.edu>
2022-04-09 12:25:44 -0500

    Added main menu and a few other small things

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,4 +2,10 @@ package sample;
 
 public class Controller
 {
+    GameEngine gameEngine;
+    public void startGame()
+    {
+        gameEngine = new GameEngine();
+        gameEngine.start();
+    }
 }
