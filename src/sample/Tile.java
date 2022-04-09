b37ef949b7d12fc3a5c98fb542641e4caf86e254
Matthew <rokusekm@lopers.unk.edu>
2022-04-09 10:53:27 -0500

    Get rid of red, nothing else

--- src/sample/Tile.java
+++ src/sample/Tile.java
@@ -3,9 +3,22 @@ package sample;
 public class Tile {
 
 
+    final float sizeX = 25f;
+    final float sizeY = 25f;
 
-    public Tile() {
+    public Tile()
+    {
 
+    }
+
+    public void setSizeX(float inSize)
+    {
+
+    }
+
+    public void setSizeY(float inSize)
+    {
+        
     }
 
 }
