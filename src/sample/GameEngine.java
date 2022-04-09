13680dd18ec874c11ab317e651bdfe2c9da99ca0
willblakemeyer <willblakemeyer@gmail.com>
2022-04-09 12:10:29 -0500

    Added timer for GameEngine & added Constants class

--- src/sample/GameEngine.java
+++ src/sample/GameEngine.java
@@ -7,11 +7,13 @@ import javafx.scene.control.Dialog;
 
 import java.util.ArrayList;
 
+import static sample.Constants.*;
+
 public class GameEngine
 {
     final float initialSize = 10;
     ArrayList<ArrayList<Tile>> tiles = new ArrayList<>();
-    ArrayList<ArrayList<Customer>> customers = new ArrayList<>();
+    ArrayList<Customer> customers = new ArrayList<>();
 
 
     public GameEngine()
@@ -37,58 +39,40 @@ public class GameEngine
         //continue here.
     }
 
-    /*
-    private class MyTimer extends AnimationTimer {
+
+    private class GameTimer extends AnimationTimer {
 
         //Keeps track of time
         long lastUpdate = 0;
+        float rate = 1000_000_000/(float)FRAME_RATE;
 
         //Keeps track of when to stop.
-        int order;
+//        int order;
 
         //constructor method.
-        public MyTimer(int order) {
-            this.order = order;
-            if (mazePane.isRunning()) {
-                mazePane.setStopped(true);
-                mazePane.setRunning(false);
-            }
-
-            if (mazePane.robot.getType() != "PortalRobot") {
-                mazePane.maze.setUsedPortal(false);
-            }
-            mazePane.maze.resetMaze();
-            mazePane.robot.resetRobot();
+        public GameTimer() {
+//            this.order = order;
         }
 
-        /
-          Clock that calls the robot method to solve the maze. Respects the rules about when to do so as well.
-          @param now
-         /
+        /**
+         * Clock that runs the game (customer AI calls, events, etc.).
+         * @param now
+         */
         @Override
         public void handle(long now) {
-            if (now - lastUpdate >= 100_000_000) {
-                if (!mazePane.robot.solved() && !mazePane.isStopped() && order == getOrder()) {
-                    lastUpdate = now;
-                    mazePane.robot.chooseMoveDirection();
-                    mazePane.paint();
-                } else {
-                    stop();
-                    mazePane.setStopped(false);
-                    mazePane.setRunning(false);
-                    Dialog<String> dialog = new Dialog<>();
+            float difference = now - lastUpdate;
 
-                    ButtonType type = new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE);
-                    dialog.getDialogPane().getButtonTypes().add(type);
+            //100 milliseconds (.1 seconds)
+            if (difference>=rate) {
 
-                    if (mazePane.robot.solved()) {
-                        dialog.setContentText(mazePane.robot.getEndMsg());
-                        dialog.setOnCloseRequest(dialogEvent -> dialog.close());
-                        dialog.show();
-                    }
+                //call customer logic every frame.
+
+                //determines if it's time to increase debt via interest.
+                for (int i=0; i<customers.size(); i++) {
+                    customers.get(i).think();
                 }
             }
         }
     }
-    */
+
 }
