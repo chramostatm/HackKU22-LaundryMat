39f9b053fc98f176fd785f52aa268a08394b5218
willblakemeyer <willblakemeyer@gmail.com>
2022-04-09 10:24:55 -0500

    Added in Tile and Customer classes & corresponding ArrayLists

---
+++ src/sample/GameEngine.java
@@ -0,0 +1,14 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class GameEngine {
+    ArrayList<ArrayList<Tile>> tiles = new ArrayList<>();
+    ArrayList<ArrayList<Customer>> customers = new ArrayList<>();
+
+
+    public GameEngine() {
+
+    }
+
+}
---
+++ src/sample/Tile.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class Tile {
+
+    public Tile() {
+
+    }
+
+}
