2918d147b3e103d19a412df79ebffafc75607b5e
willblakemeyer <willblakemeyer@gmail.com>
2022-04-09 12:59:29 -0500

    Filling out Machine.java variables, getters, and setters

--- src/sample/Machine.java
+++ src/sample/Machine.java
@@ -2,9 +2,28 @@ package sample;
 
 public class Machine extends Tile
 {
+    private boolean available = false;
+    private boolean active = false;
+    private int timeUntilComplete = 0;
 
     public Machine(float inX,float inY) {
         super(inX, inY);
+    }
+
+    public boolean getAvailable() {
+        return this.available;
+    }
+
+    public boolean getActive() {
+        return this.active;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 
 }
