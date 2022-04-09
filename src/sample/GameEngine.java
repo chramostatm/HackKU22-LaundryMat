70d037052517bd70958ba9481329c9f07e8ccdce
willblakemeyer <willblakemeyer@gmail.com>
2022-04-09 10:43:59 -0500

    Added in AnimationTimer example for Justin

--- src/sample/GameEngine.java
+++ src/sample/GameEngine.java
@@ -1,5 +1,10 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+
 import java.util.ArrayList;
 
 public class GameEngine {
@@ -11,4 +16,58 @@ public class GameEngine {
 
     }
 
+    /*
+    private class MyTimer extends AnimationTimer {
+
+        //Keeps track of time
+        long lastUpdate = 0;
+
+        //Keeps track of when to stop.
+        int order;
+
+        //constructor method.
+        public MyTimer(int order) {
+            this.order = order;
+            if (mazePane.isRunning()) {
+                mazePane.setStopped(true);
+                mazePane.setRunning(false);
+            }
+
+            if (mazePane.robot.getType() != "PortalRobot") {
+                mazePane.maze.setUsedPortal(false);
+            }
+            mazePane.maze.resetMaze();
+            mazePane.robot.resetRobot();
+        }
+
+        /
+          Clock that calls the robot method to solve the maze. Respects the rules about when to do so as well.
+          @param now
+         /
+        @Override
+        public void handle(long now) {
+            if (now - lastUpdate >= 100_000_000) {
+                if (!mazePane.robot.solved() && !mazePane.isStopped() && order == getOrder()) {
+                    lastUpdate = now;
+                    mazePane.robot.chooseMoveDirection();
+                    mazePane.paint();
+                } else {
+                    stop();
+                    mazePane.setStopped(false);
+                    mazePane.setRunning(false);
+                    Dialog<String> dialog = new Dialog<>();
+
+                    ButtonType type = new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE);
+                    dialog.getDialogPane().getButtonTypes().add(type);
+
+                    if (mazePane.robot.solved()) {
+                        dialog.setContentText(mazePane.robot.getEndMsg());
+                        dialog.setOnCloseRequest(dialogEvent -> dialog.close());
+                        dialog.show();
+                    }
+                }
+            }
+        }
+    }
+    */
 }
