4bf34d0b98d4b6186f17d94c02ef72e137228c29
willblakemeyer <willblakemeyer@gmail.com>
2022-04-10 06:13:59 -0500

    Fixed unfinished copy/paste job

--- src/sample/Graphics.java
+++ src/sample/Graphics.java
@@ -123,13 +123,13 @@ public class Graphics
         bigButtonBox.getChildren().addAll(buttonBox, pay50);
 
         Button advertise = new Button();
-        pay50.setText("Advertise!");
-        pay50.setOnAction(event ->
+        advertise.setText("Advertise!");
+        advertise.setOnAction(event ->
         {
             GameController.gameEngine.speedModifier+=0.1F;
 
         });
-        bigButtonBox.getChildren().addAll(buttonBox, pay50);
+        bigButtonBox.getChildren().addAll(buttonBox, pay50, advertise);
 
         repair.setOnAction(event ->
         {
